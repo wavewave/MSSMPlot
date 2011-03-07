0fcc8bc3ca182a701d22ee164cd0a8729967ff04
ianwookim <ianwookim@gmail.com>
2011-03-07 01:12:46 +0000

    draw all the lines not latex yet.

--- src/HEP/Physics/MSSM/Spectrum/Plot.hs
+++ src/HEP/Physics/MSSM/Spectrum/Plot.hs
@@ -1,14 +1,91 @@
+{-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
+
 module HEP.Physics.MSSM.Spectrum.Plot where
+
+import Control.Monad
 
 import HROOT
 import HROOT.Type
 import HROOT.Function
 import HROOT.Class
 
+import HEP.Physics.MSSM.Type
+  
 
-helloworld :: IO ()
-helloworld = do 
+helloworld :: OutputPhys -> IO ()
+helloworld OutputPhys {..} = do 
   putStrLn "hi"
+  
+  let ymin = 0 
+      ymax = 1000
+      
   c1 <- newTCanvas "test" "test" 640 480  
+  
+  setBorderMode c1 0 
+  setFillStyle  c1 1001
+  setFillColor  c1 10
+  
+  range1 <- newTH2F "test" "test" 100 0.0 130.0 100 ymin ymax 
+  draw range1 "" 
+  
+  xaxis <- getXaxis range1 
+  yaxis <- getYaxis range1
+  
+  setTickLength xaxis 0 
+  setLabelSize xaxis 0 
+  setTitle yaxis "Mass ( GeV )" 
+  setTitleOffset yaxis 1.25
+  setNdivisions yaxis 505 1
+  
+
+  l_h   <- newTLine  10.0 mass_Mh 20.0 mass_Mh 
+  la_h  <- newTLatex 14.0 (mass_Mh+45) "h" 
+  
+  l_A   <- newTLine  10.0 mass_MH3 20.0 mass_MH3
+  la_A  <- newTLatex 10.0 (mass_MH3+45) "A"  
+  
+  l_H   <- newTLine  10.0 mass_MHH 20.0 mass_MHH
+  la_H  <- newTLatex 14.0 (mass_MHH+45) "H"
+  
+  l_Hp  <- newTLine  10.0 mass_MHc 20.0 mass_MHc
+  la_Hp <- newTLatex 18.0 (mass_MHc+45) "H^{#pm}"
+
+  ls_neut  <- forM [mass_MNE1,mass_MNE2,mass_MNE3,mass_MNE4] $ 
+                \x -> newTLine 30.0 x 40.0 x 
+  la_neut1 <- newTLatex 30.0 (mass_MNE1+45) "#chi_{i}^{0}"
+  
+  ls_ch    <- forM [mass_MC1,mass_MC2] $
+                \x -> newTLine 50.0 x 60.0 x 
+
+  l_gluino <- newTLine 70.0 mass_MSG 80.0 mass_MSG
+  
+  [l_u11,l_d11,l_e11,l_snu1] <- 
+    forM [mass_MSuL, mass_MSdL, mass_MSeL, mass_MSne ] $ 
+      \x -> newTLine 90.0 x 94.0 x 
+
+  [l_u12,l_d12,l_e12] <- 
+    forM [mass_MSuR, mass_MSdR, mass_MSeR ] $ 
+      \x -> newTLine 96.0 x 100.0 x 
+
+  [l_u31,l_d31,l_e31,l_snu3] <- 
+    forM [mass_MSt1, mass_MSb1, mass_MSl1, mass_MSn1 ] $ 
+      \x -> newTLine 110.0 x 114.0 x 
+
+  [l_u32,l_d32,l_e32] <- 
+    forM [mass_MSt2, mass_MSb2, mass_MSl2 ] $ 
+      \x -> newTLine 116.0 x 120.0 x 
+
+
+  mapM_ (flip draw "")  ( [ l_h, l_A, l_H, l_Hp ] 
+                          ++ ls_neut ++ ls_ch ++ [l_gluino]
+                          ++ [l_u11,l_d11,l_e11,l_snu1] 
+                          ++ [l_u12,l_d12,l_e12] 
+                          ++ [l_u31,l_d31,l_e31,l_snu3] 
+                          ++ [l_u32,l_d32,l_e32] )
+    
+                       
+  mapM_ (flip draw "")  [la_h, la_A, la_H, la_Hp ] 
+
+
   saveAs c1 "test.pdf" ""  
   
--- test/test.hs
+++ test/test.hs
@@ -1,6 +1,52 @@
 module Main where
 
+import HEP.Physics.MSSM.Type
 import HEP.Physics.MSSM.Spectrum.Plot
 
+testphys = OutputPhys {
+  mass_Mh   = 100.0,
+  mass_MHH  = 105.0, 
+  mass_MH3  = 300,
+  mass_MHc  = 340, 
+  mass_MNE1 = 120, 
+  mass_MNE2 = 240,
+  mass_MNE3 = 308, 
+  mass_MNE4 = 500, 
+  mass_MC1  = 500, 
+  mass_MC2  = 250, 
+  mass_MSG  = 700, 
+  mass_MSuL = 350, 
+  mass_MSdL = 690, 
+  mass_MSeL = 400, 
+  mass_MSne = 340, 
+  mass_MSuR = 320, 
+  mass_MSdR = 820, 
+  mass_MSeR = 400, 
+  mass_MScL = 440,
+  mass_MSsL = 460, 
+  mass_MSmL = 120, 
+  mass_MSnm = 130, 
+  mass_MScR = 140, 
+  mass_MSsR = 150, 
+  mass_MSmR = 160,
+  mass_MSt1 = 490, 
+  mass_MSb1 = 393, 
+  mass_MSl1 = 728, 
+  mass_MSn1 = 138, 
+  mass_MSt2 = 240, 
+  mass_MSb2 = 250,
+  mass_MSl2 = 440, 
+  deltarho  = 0.1, 
+  gmuon     = 0.9, 
+  bsgnlo    = 7.5, 
+  bsmumu    = 3.3, 
+  bino      = 0.1, 
+  wino      = 0.5, 
+  higgsino1 = 0.9, 
+  higgsino2 = 0.8, 
+  micro_Xf  = 30, 
+  micro_Omega = 0.03 
+} 
+
 main = do 
-  helloworld
+  helloworld testphys
